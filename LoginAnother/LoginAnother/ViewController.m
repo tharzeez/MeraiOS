e7ecca3e1629883639e89a4fd3d2d939c427e119
Tharzeez <tharzeez@Tharzeezs-MacBook-Pro.local>
2017-07-10 17:43:12 +0530

    Updated App

---
+++ LoginAnother/LoginAnother/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LoginAnother
+//
+//  Created by Tharzeez on 7/10/17.
+//  Copyright © 2017 Tharzeez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ LoginAnother/LoginAnother/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoginAnother
+//
+//  Created by Tharzeez on 7/10/17.
+//  Copyright © 2017 Tharzeez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
