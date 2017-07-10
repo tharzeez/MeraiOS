f03e3f352b0cabe2079c838b929a8ca1035ec98b
Tharzeez <tharzeez@Tharzeezs-MacBook-Pro.local>
2017-07-10 17:54:30 +0530

    Updates

---
+++ LoginAnother/LoginAnother/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginAnother
+//
+//  Created by Tharzeez on 7/10/17.
+//  Copyright © 2017 Tharzeez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginAnother/LoginAnotherTests/LoginAnotherTests.m
@@ -0,0 +1,39 @@
+//
+//  LoginAnotherTests.m
+//  LoginAnotherTests
+//
+//  Created by Tharzeez on 7/10/17.
+//  Copyright © 2017 Tharzeez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LoginAnotherTests : XCTestCase
+
+@end
+
+@implementation LoginAnotherTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
